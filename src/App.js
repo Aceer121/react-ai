e096c5a79f909a5a182bdd26842c77a3c948d626
Arcel Derosena <45231128+Aceer121@users.noreply.github.com>
2024-04-06 00:07:22 -0400

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
-// App.js
 import React from 'react';
-import { BrowserRouter as Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+
 import Home from './components/Home';
 import About from './components/About';
 import SignIn from './components/SignIn';
 
 function App() {
+  const buttonStyle = {
+    color: 'white',
+    textDecoration: 'none',
+    fontSize: '25px', // Adjust the font size as needed
+    margin: '0 10px',
+  };
+
   return (
-    <Routes>
-      <div className="App" style={{ backgroundColor: 'black', height: '100vh', display: 'flex', 
-            flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+    <Router>
+      <div className="App" style={{ backgroundColor: 'black', height: '100vh', position: 'relative', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
         <h1 style={{ color: 'white' }}>React AI</h1>
-        <nav>
-          <ul style={{ listStyleType: 'none', padding: 0 }}>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/signin">Sign In</Link></li>
-          </ul>
-        </nav>
-        
+        <div style={{ textAlign: 'center' }}>
+          <input type="text" placeholder="Search..." style={{ width: '300px', padding: '10px', borderRadius: '5px', border: '2px solid #ccc', backgroundColor: 'white' }} />
+        </div>
+        <div style={{ position: 'absolute', top: 0, right: 0 }}>
+          <nav>
+            <ul style={{ listStyleType: 'none', padding: 0, display: 'flex' }}>
+              <li style={{ marginRight: '10px' }}><Link to="/" style={buttonStyle}>Home</Link></li>
+              <li style={{ marginRight: '10px' }}><Link to="/about" style={buttonStyle}>About</Link></li>
+              <li><Link to="/signin" style={buttonStyle}>Sign In</Link></li>
+            </ul>
+          </nav>
+        </div>
+      
+        <Routes>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/signin" component={SignIn} />
+        </Routes>
 
       </div>
-    </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/SignIn/SignIn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SignIn() {
+  return (
+    <div>
+      <h2>Sign In</h2>
+      <p>Please sign in to continue.</p>
+    </div>
+  );
+}
+
+export default SignIn;
