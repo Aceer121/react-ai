b910287e203d8b64b0c6b03fd902d7814ee79814
Arcel Derosena <45231128+Aceer121@users.noreply.github.com>
2024-04-18 17:49:11 -0400

    Black page

--- src/App.js
+++ src/App.js
@@ -7,39 +7,16 @@ import About from './components/About';
 import SignIn from './components/SignIn';
 
 function App() {
-  const buttonStyle = {
-    color: 'white',
-    textDecoration: 'none',
-    fontSize: '25px', // Adjust the font size as needed
-    margin: '0 10px',
-  };
-
-  return (
+   return (
+    <div className='App'>
     <Router>
-      <div className="App" style={{ backgroundColor: 'black', height: '100vh', position: 'relative', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-        <h1 style={{ color: 'white' }}>Arcel's React AI</h1>
-        <div style={{ textAlign: 'center' }}>
-          <input type="text" placeholder="Search React Ai..." style={{ width: '300px', padding: '10px', borderRadius: '5px', border: '2px solid #ccc', backgroundColor: 'white' }} />
-        </div>
-        <div style={{ position: 'absolute', top: 0, right: 0 }}>
-          <nav>
-            <ul style={{ listStyleType: 'none', padding: 0, display: 'flex' }}>
-              <li style={{ marginRight: '10px' }}><Link to="/" style={buttonStyle}>Home</Link></li>
-              <li style={{ marginRight: '10px' }}><Link to="/about" style={buttonStyle}>About</Link></li>
-              <li><Link to="/signin" style={buttonStyle}>Sign In</Link></li>
-            </ul>
-          </nav>
-        </div>
-      
         <Routes>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/signin" component={SignIn} />
         </Routes>
-
-      </div>
     </Router>
+    </div> 
   );
-}
-
+ }
 export default App;
